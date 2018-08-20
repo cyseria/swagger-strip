97ae939425f984eeeb8a63fd71096b8a75edfa00
cyseria <xcyseria@gmail.com>
2018-08-20 14:50:24 +0800

    🏁  test: add test

--- __tests__/main.test.js
+++ __tests__/main.test.js
@@ -1,10 +1,55 @@
 /**
- * @file main
- * @author imcuttle
- * @date 2018/4/4
+ * @file main test
+ * @author cyseria
  */
-const swaggerStrip = require('../index')
 
-test('strip swagger', ()=> {
-  expect(swaggerStrip()).toBe('123');
+const fs = require('fs');
+const path = require('path');
+const yaml = require('js-yaml');
+const swaggerStrip = require('..');
+
+const sourceJsonFile = path.join(__dirname, './data/test.json');
+const sourceCode = fs.readFileSync(sourceJsonFile, 'utf8');
+const sourceObject = JSON.parse(sourceCode);
+
+const wishPetRefs = {
+    '#/definitions/Pet': sourceObject['definitions']['Pet'],
+    '#/definitions/Category': sourceObject['definitions']['Category'],
+    '#/definitions/Tag': sourceObject['definitions']['Tag']
+};
+const wishPetTags = [sourceObject['tags'][0]];
+
+describe('main test', () => {
+    it('should strip "/pet/findByStatus" api from source object', () => {
+        const stripResult = swaggerStrip(sourceObject, '/pet/findByStatus');
+        const wishPetFindByStatusPaths = sourceObject['paths']['/pet/findByStatus']['get'];
+        expect(stripResult.paths).toEqual(wishPetFindByStatusPaths);
+        expect(stripResult.refs).toEqual(wishPetRefs);
+        expect(stripResult.tags).toEqual(wishPetTags);
+    });
+
+    it('should strip custom method from source object', () => {
+        const stripResult = swaggerStrip(sourceObject, '/pet', {
+            method: 'post'
+        });
+        const wishPetPaths = sourceObject['paths']['/pet']['post'];
+        expect(stripResult.paths).toEqual(wishPetPaths);
+        expect(stripResult.refs).toEqual(wishPetRefs);
+        expect(stripResult.tags).toEqual(wishPetTags);
+    });
 });
+
+describe('files test', () => {
+    it('should strip custom method from yml file', () => {
+        const sourceYmlFile = path.join(__dirname, './data/test.yml');
+        const sourceCode = fs.readFileSync(sourceYmlFile, 'utf8');
+        const sourceObject = yaml.safeLoad(sourceCode);
+        const stripResult = swaggerStrip(sourceObject, '/pet', {
+            method: 'post'
+        });
+        const wishPetPaths = sourceObject['paths']['/pet']['post'];
+        expect(stripResult.paths).toEqual(wishPetPaths);
+        expect(stripResult.refs).toEqual(wishPetRefs);
+        expect(stripResult.tags).toEqual(wishPetTags);
+    });
+})
